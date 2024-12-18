07285bdbaaf92f376c3b9b28c5842d42d6e33f3d
hemanthhvv <vvhemanthh@gmail.com>
2024-12-18 22:23:44 +0530

    parsing logic fune tuned

--- agriapplication/imageUploader/views.py
+++ agriapplication/imageUploader/views.py
@@ -2,21 +2,45 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.core.files.storage import FileSystemStorage
+from django.conf import settings
 import os
 from .utils import ProcessImage
 from .models import UploadedData
+from django.contrib import messages
 
 processor = ProcessImage()
+
+def deleteImage(request,uuid:str):
+    if request.method == 'POST':
+        getData = UploadedData.objects.get(user=request.user,uuid=uuid)
+        path_of_media = os.path.join(settings.BASE_DIR,'media',str(getData.image))
+        os.remove(path_of_media)
+        getData.delete()
+        messages.error(request,message=f"File ID {uuid} deleted sucesssfully")
+        return redirect('home')
+
+def showMap(request):
+    coodinates = UploadedData.objects.all()
+    context = {
+        'images':coodinates
+    }
+    # import pdb
+    # pdb.set_trace()
+    return render(request,'imageUploader/map.html',context=context)
+
 
 @login_required(login_url='/authentication/login')
 def index(request):
     images = UploadedData.objects.all()
-    details = UploadedData.objects.values_list('details')
+
+    if not images:
+        return render(request=request,template_name='imageUploader/empty.html')
 
     context = {
         "images":images,
-        "details":details
     }
+    # import pdb
+    # pdb.set_trace()
     return render(request,'imageUploader/index.html',context=context)
 
 def add_image(request):
@@ -44,24 +68,23 @@ def add_image(request):
         else:
             return "Unable to parse the image"
 
-        import pdb
-        pdb.set_trace()
-
-        latitude = coordinates[0]
-        longitude = coordinates[1]
-        details = '?'.join(listValues[2:])
+        latitude = coordinates[0].replace(' ','').split(':')[1]
+        longitude = coordinates[1].replace(' ','').split(':')[1]
+        timeOfPhoto,farmerID,farmerName = processor.makePreciseText(coordinates[2:])
 
         UploadedData.objects.create(
             image = image,
             latitude = latitude,
             longitude = longitude,
-            details = details,
+            timeOfPhoto = timeOfPhoto,
+            farmerID = farmerID,
+            farmerName = farmerName,
             user= request.user
         )
         print('Completed Processing')
         print('Saved')
 
         os.remove(temp_path)
-
+        messages.success(request,"File added sucessfully")
         return redirect('home')  # Redirect after saving
     return render(request, 'imageUploader/uploader.html')
