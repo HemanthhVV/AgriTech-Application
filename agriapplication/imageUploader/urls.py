6a72d36e6241e56a86c97233e2993b7398f645c5
hemanthhvv <vvhemanthh@gmail.com>
2024-12-18 22:23:19 +0530

    delete functionalities

--- agriapplication/imageUploader/urls.py
+++ agriapplication/imageUploader/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="home"),
-    path("imageupload/", views.add_image, name="imageupload")
+    path("imageupload/", views.add_image, name="imageupload"),
+    path("delete/<str:uuid>", views.deleteImage, name="delete"),
+    path("showingMap/", views.showMap, name="showmap")
 ]
